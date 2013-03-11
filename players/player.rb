a02960bf30d7347e1709ad07a895017645f7339c
Eve Barabas <b_seven_e@yahoo.co.uk>
2013-03-11 20:48:51 +0000

    third try

--- players/player.rb
+++ players/player.rb
@@ -8,8 +8,12 @@ class Player
     # MyHelper.answer
     vowels = ["e", "a", "i", "o", "u", "y"]
     consonants =  ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x"]
-    current_vowel = (guesses.size > vowels.size) ? (guesses - vowels).sample : (vowels - guesses).sample
-    current_consonant = (guesses.size > consonants.size) ? (guesses - consonants).sample : (consonants - guesses).sample
-    return Array.new(current_vowel, current_consonant).sample
+    possible_vowels = vowels - guesses
+    possible_consonants = consonants - guesses
+    
+    vowel = possible_vowels.sample unless possible_vowels.empty?
+    consonant = possible_consonants.sample unless possible_consonants.empty?
+    
+    return vowel || consonant
   end
 end
--- players/spec/player_spec.rb
+++ players/spec/player_spec.rb
@@ -6,6 +6,5 @@ describe "Player" do
     let (:vowels) { ["e", "a", "i", "o", "u"] }
     let (:constants) { ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x"] }
     subject { lambda { |guesses| Player.take_turn(state, guesses) } }
-  
   end
 end
